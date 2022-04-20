4f769530370f8408fce1ed03ff610e204442a28e
CloneOrNot <83652142+CloneOrNot@users.noreply.github.com>
2022-04-20 23:39:39 +0200

    Query Removed

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -5,6 +5,7 @@ import { AuthService } from 'src/app/shared/services/auth.service';
 
 
 // TODO Edit/ Save image
+// TODO Partially disable edit for google kontos
 
 
 @Component({
@@ -17,13 +18,7 @@ export class ProfileComponent implements OnInit {
 
   userData = JSON.parse(localStorage.getItem('user')!);
 
-
-  // revert changes of editing
-  public resetEdit(){
-
-    document.getElementById("username")!.innerText = this.userData.displayName;
-    document.getElementById("email")!.innerText = this.userData.email;
-  }
+  editActive = false;
 
 
   public saveEdit(){
@@ -31,25 +26,8 @@ export class ProfileComponent implements OnInit {
     // TODO Save Edit
   }
 
-
-  // make text editable and change showon buttons
-  public swapEdit(){
-
-    var elementList = document.querySelectorAll(".editables");
-
-    elementList.forEach(element => {
-      element.setAttribute("contenteditable",
-      element.getAttribute("contenteditable") == "true" ? "false" : "true");
-    });
-
-
-    var buttonList = document.querySelectorAll(".editablesButton");
-
-    buttonList.forEach(button => {
-      button.classList.contains("hidden") ?
-        button.classList.remove("hidden") :
-        button.classList.add("hidden");
-    });
+  public getData(){
+    return JSON.parse(localStorage.getItem('user')!);
   }
 
   ngOnInit(): void {}
