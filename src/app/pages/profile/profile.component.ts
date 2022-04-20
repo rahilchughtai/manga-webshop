cddf7a2a0dd53f79fb1df8b2a67ae014e90f45ed
CloneOrNot <83652142+CloneOrNot@users.noreply.github.com>
2022-04-20 20:12:58 +0200

    show and basic edit functions

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
 
 import { AuthService } from 'src/app/shared/services/auth.service';
+
+
 
 @Component({
   selector: 'app-profile',
@@ -9,6 +12,25 @@ import { AuthService } from 'src/app/shared/services/auth.service';
 })
 export class ProfileComponent implements OnInit {
   constructor(public authService: AuthService) {}
+
+  userData = JSON.parse(localStorage.getItem('user')!);
+
+  public changedUserData = JSON.parse(localStorage.getItem('user')!);
+
+  public updateChangedUserData(text : any, position : any){
+    this.changedUserData[position] = text.target.innerText;
+    console.log(this.changedUserData)
+  }
+
+  public swapEdit(){
+
+    var elementList = document.querySelectorAll(".editables");
+
+    elementList.forEach(element => {
+      element.setAttribute("contenteditable",
+      element.getAttribute("contenteditable") == "true" ? "false" : "true");
+    });
+  }
 
   ngOnInit(): void {}
 
