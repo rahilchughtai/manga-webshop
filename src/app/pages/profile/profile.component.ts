efaee90aea7078bdb782b0fbac8052497f6e6f13
CloneOrNot <83652142+CloneOrNot@users.noreply.github.com>
2022-04-23 01:33:06 +0200

    UI Updated & Clean Up

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/shared/services/auth.service';
 
-// TODO scss/ style
 // TODO Edit/ Save image
-// TODO Disable edit for google kontos - Edit image
+//          Disable Image edit for google kontos
 
 @Component({
   selector: 'app-profile',
@@ -14,29 +13,14 @@ export class ProfileComponent implements OnInit {
   constructor(public authService: AuthService) {}
 
   editActive : Boolean= false;
-  errorInEditInput : Boolean= false;
+
+  // TODO Check if Inputs are correct. If no: <mat-error>
+  errorInEdit : Boolean= false;
 
    // restes the shown profile data to pre edit
   resetData() : void{
     this.authService.userData = JSON.parse(localStorage.getItem('user')!);
   }
-
-  checkIfUserDataIsCorrect() : Boolean{
-    var localData : any = this.authService.userData
-
-    if(typeof localData.adress.streetNumber != "number" || localData.adress.streetNumber == ""
-      || localData.adress.streetName == ""
-      || typeof localData.adress.plz != "number" || localData.adress.plz == ""
-      || localData.adress.ort == ""
-      || localData.adress.country == ""
-      || localData.firstName == ""
-      || localData.secondName == ""
-      || localData.displayName == ""){
-      return false
-    }
-    return true
-  }
-
 
   ngOnInit(): void {}
 
