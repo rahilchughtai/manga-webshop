a2b67a1c6089c97d0bb1c6c33c07c290f3db63b0
CloneOrNot <83652142+CloneOrNot@users.noreply.github.com>
2022-04-24 19:03:00 +0200

    Init - Search on Home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  JikanApiResponse,
+  Pagination,
+} from 'src/app/shared/models/response.model';
+import { Observable, filter, first, map, share, take } from 'rxjs';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
+
+import { FormControl } from '@angular/forms';
+import { MangaApiService } from 'src/app/shared/services/manga-api.service';
+import { MangaItem } from 'src/app/shared/models/manga-item.model';
+import { PageEvent } from '@angular/material/paginator';
+import data from 'src/assets/response.json';
 
 @Component({
   selector: 'page-home',
@@ -6,7 +18,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
+  constructor(private mangaApi: MangaApiService) {}
 
-  ngOnInit(): void {}
+  JikanApiResponse$!: Observable<JikanApiResponse>;
+  gridColumns = 5;
+  value = '';
+  pageIndex = 0;
+  totalRecords = 0;
+  pageSize = 0;
+  hasNext = false;
+  paginationData!: Pagination;
+
+  ngOnInit(): void {
+    this.fetchMangaApiData();
+  }
+
+  fetchMangaApiData(term?: string, index?: number, limit?: number) {
+    this.JikanApiResponse$ = this.mangaApi
+      .getJikanMangaData(term, index, 3)
+      .pipe(share());
+    this.mapToPageValue();
+  }
+
+  mapToPageValue() {
+    this.JikanApiResponse$.pipe(
+      map((resp) => resp.pagination),
+      take(1)
+    ).subscribe((pag: Pagination) => {
+      this.initializePageValues(pag);
+    });
+  }
+
+  initializePageValues(pag: Pagination) {
+    const { items } = pag;
+    ({ current_page: this.pageIndex } = pag);
+    ({ total: this.totalRecords, count: this.pageSize } = items);
+  }
+
+  trackByFn(index: number, item: MangaItem) {
+    return item.mal_id;
+  }
 }
