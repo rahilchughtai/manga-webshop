3bddf4e5c0500430db679d8647021ee76137a71b
rahilchughtai <rahilchughtai1@gmail.com>
2022-04-25 23:43:11 +0200

    removing string interpolated styling

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  JikanApiResponse,
-} from 'src/app/shared/models/response.model';
 import { Observable, share } from 'rxjs';
 
+import { JikanApiResponse } from 'src/app/shared/models/response.model';
 import { MangaApiService } from 'src/app/shared/services/manga-api.service';
 import { MangaItem } from 'src/app/shared/models/manga-item.model';
-
-
 
 @Component({
   selector: 'page-home',
@@ -20,29 +16,33 @@ export class HomeComponent implements OnInit {
   JikanApiResponse$!: Observable<JikanApiResponse>;
 
   loadedElements = 15;
-  width = "width: " + this.widthCalc() + "px";
+  width = 'width: ' + this.widthCalc() + 'px';
   elementsMoved = 0;
-  elementsMoved_style = "left: 0px;";
-  elementInBox = Math.max(Math.min(Math.floor(((window.innerWidth * 0.8) / 312)), 10),1);
+  leftStyleProperty = 0;
+  shiftValue = 312;
 
-  widthCalc() : number{
-    this.elementInBox = Math.max(Math.min(Math.floor(((window.innerWidth * 0.8) / 312)), 10),1)
-    return this.elementInBox * 312
+  elementInBox = Math.max(
+    Math.min(Math.floor((window.innerWidth * 0.8) / 312), 10),
+    1
+  );
+
+  widthCalc(): number {
+    this.elementInBox = Math.max(
+      Math.min(Math.floor((window.innerWidth * 0.8) / 312), 10),
+      1
+    );
+    return this.elementInBox * 312;
   }
 
-  listenWidthChange() : void {
-    this.width = "width: " + this.widthCalc() + "px";
+  listenWidthChange(): void {
+    this.width = 'width: ' + this.widthCalc() + 'px';
   }
 
-  moveElementLeft() : void {
-    this.elementsMoved --;
-    this.elementsMoved_style = "left: " + (this.elementsMoved * -312) + "px";
+  moveElement(right: boolean) {
+    const shift = right ? 1 : -1;
+    this.elementsMoved += shift;
+    this.leftStyleProperty = this.elementsMoved * -312;
   }
-  moveElementRight() : void {
-    this.elementsMoved ++;
-    this.elementsMoved_style = "left: " + (this.elementsMoved * -312) + "px";
-  }
-
   ngOnInit(): void {
     this.fetchMangaApiData();
   }
