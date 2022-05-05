a6e8996f2cfd6acb80fa927ec034f7ed824c2aa7
rahilchughtai <rahilchughtai1@gmail.com>
2022-05-05 15:46:25 +0200

    clean up

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -8,19 +8,6 @@ import { CartService } from 'src/app/shared/services/cart.service';
 import { MatSelectChange } from '@angular/material/select';
 import { makeNumbersArray } from 'src/app/shared/utils/manga-utils';
 
-const ELEMENT_DATA = [
-  { image: 1, name: 'Hydrogen', quantity: 1.0079, total: 'H' },
-  { image: 2, name: 'Helium', quantity: 4.0026, total: 'He' },
-  { image: 3, name: 'Lithium', quantity: 6.941, total: 'Li' },
-  { image: 4, name: 'Beryllium', quantity: 9.0122, total: 'Be' },
-  { image: 5, name: 'Boron', quantity: 10.811, total: 'B' },
-  { image: 6, name: 'Carbon', quantity: 12.0107, total: 'C' },
-  { image: 7, name: 'Nitrogen', quantity: 14.0067, total: 'N' },
-  { image: 8, name: 'Oxygen', quantity: 15.9994, total: 'O' },
-  { image: 9, name: 'Fluorine', quantity: 18.9984, total: 'F' },
-  { image: 10, name: 'Neon', quantity: 20.1797, total: 'Ne' },
-];
-
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
@@ -33,10 +20,6 @@ export class CartComponent implements OnInit {
   ) {}
 
   makeArray = makeNumbersArray;
-  initialQuantities: number[] = [];
-  displayedColumns: string[] = ['', 'name', 'quantity', 'total'];
-  dataSource = ELEMENT_DATA;
-  initialQuantity = 5;
   IncDec = CartIncDec;
   shoppingCartData: Observable<CartItem[]> | undefined = undefined;
   OrderTotal: Observable<number> | undefined = undefined;
