07f1064902fd95914619847244cdfb897be69934
rahilchughtai <rahilchughtai1@gmail.com>
2022-05-06 11:19:59 +0200

    AuthGuard change, prevent login page visit when already logged in

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,31 +1,57 @@
-import {RouterModule, Routes} from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterModule,
+  RouterStateSnapshot,
+  Routes,
+} from '@angular/router';
+import { Injectable, NgModule } from '@angular/core';
 
-import {CartComponent} from './pages/cart/cart.component';
-import {FavoritesComponent} from './pages/favorites/favorites.component';
-import {HomeComponent} from './pages/home/home.component';
-import {LoginRegisterComponent} from './pages/login-register/login-register.component';
-import {MangaDetailComponent} from './pages/manga-detail/manga-detail.component';
-import {MangaListComponent} from './pages/manga-list/manga-list.component';
-import {NgModule} from '@angular/core';
-import {ProfileComponent} from "./pages/profile/profile.component";
+import { AuthService } from './shared/services/auth.service';
+import { CartComponent } from './pages/cart/cart.component';
+import { FavoritesComponent } from './pages/favorites/favorites.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginRegisterComponent } from './pages/login-register/login-register.component';
+import { MangaDetailComponent } from './pages/manga-detail/manga-detail.component';
+import { MangaListComponent } from './pages/manga-list/manga-list.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  constructor(private auth: AuthService, private router: Router) {}
+
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    if (this.auth.isLoggedIn) {
+      this.router.navigateByUrl('/home');
+      return false;
+    }
+    return true;
+  }
+}
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'home', component: HomeComponent},
-  {path: 'profile', component: ProfileComponent},
-  {path: 'search', component: MangaListComponent},
-  {path: 'favorites', component: FavoritesComponent},
-  {path: 'cart', component: CartComponent},
-  {path: 'search', component: MangaListComponent},
-  {path: 'login', component: LoginRegisterComponent},
-  {path: 'manga/:mid', component: MangaDetailComponent},
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'search', component: MangaListComponent },
+  { path: 'favorites', component: FavoritesComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'search', component: MangaListComponent },
+  {
+    path: 'login',
+    component: LoginRegisterComponent,
+    canActivate: [AuthGuard],
+  },
+  { path: 'manga/:mid', component: MangaDetailComponent },
 
-  {path: '**', component: HomeComponent},
+  { path: '**', component: HomeComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule {}
