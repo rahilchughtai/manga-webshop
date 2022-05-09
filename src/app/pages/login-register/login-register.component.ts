7ce6736a32b13e917ec0a3b7f3b71e4aa13c2acf
rahilchughtai <rahilchughtai1@gmail.com>
2022-05-09 22:28:30 +0200

    first steps

--- src/app/pages/login-register/login-register.component.ts
+++ src/app/pages/login-register/login-register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { AuthService } from 'src/app/shared/services/auth.service';
 
@@ -8,7 +9,16 @@ import { AuthService } from 'src/app/shared/services/auth.service';
   styleUrls: ['./login-register.component.scss'],
 })
 export class LoginRegisterComponent implements OnInit {
-  constructor(public authService: AuthService) {}
+  constructor(public authService: AuthService, public fb: FormBuilder) {}
 
+  loginForm: FormGroup = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  LoginWithEmail() {
+    
+
+  }
   ngOnInit(): void {}
 }
