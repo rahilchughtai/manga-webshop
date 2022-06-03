8fec974cabbb288f37037a9c8f80e4187382c42d
CloneOrNot <83652142+CloneOrNot@users.noreply.github.com>
2022-06-03 09:35:41 +0200

    Update 1
    - touch event fix
    - Kommentare
    - fix detailed view (mobile)
    - OFFEN: Bestellungen

--- src/app/components/comment-list/user-comment.component.ts
+++ src/app/components/comment-list/user-comment.component.ts
@@ -7,17 +7,25 @@ import { MangaComment } from 'src/app/shared/models/comment.model';
   template: `
     <div>
       <p>
-        {{ comment.userData.displayName }} <br />
+        <span>{{ comment.userData.displayName }}</span> <br />
         {{ comment.text }}
       </p>
     </div>
   `,
   styles: [
     `
+      @use "../../shared/styles/customvars" as *;
       p {
+        background-color: $main_color_dark;
         padding: 1em;
         width: 50vw;
+        min-width: 250px;
         word-break: break-word;
+
+        border-radius: 5px;
+      }
+      span  {
+        opacity: 0.8;
       }
     `,
   ],
